a435e1bf3fdf3c6330847795cb4757c583c73693
Hoang Triet Thong Le <lehoangtrietthong2102004@gmail.com>
2025-01-20 11:37:22 +0700

    create a simple to do list

--- To Do List/to-do-list/app.py
+++ To Do List/to-do-list/app.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-
+import logging
+import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'db.sqlite3')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+with app.app_context():
+    try:
+        db.create_all()
+    except Exception as e:
+        logging.error(f"Failed to create database: {str(e)}")
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -24,7 +32,8 @@ def add():
         db.session.add(new_task)
         db.session.commit()
         return redirect('/')
-    except:
+    except Exception as e:
+        logging.error(f"Database error: {str(e)}")
         return 'There was an issue adding your task'
     
 #Route to delete a task
@@ -38,26 +47,12 @@ def delete(id):
     except:
         return 'There was a problem deleting that task'
     
-#Route to update a task
-@app.route('/update/<int:id>', methods=['POST', 'GET'])
-def update(id):
-    task = Todo.query.get_or_404(id)
-    if request.method == 'POST':
-        task.content = request.form['content']
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating your task'
-    else:
-        return render_template('update.html', task=task)
-
 
 #Route to display all tasks
 @app.route('/')
 def index():
     To_do_list = Todo.query.all()
-    return render_template('base.html', To_do_list=To_do_list)
+    return render_template('index.html', To_do_list=To_do_list)
 
     
 
